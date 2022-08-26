c441585536a4ff60fb979cf09d26deaf506bc5fc
n1colashka <deckards2505@gmail.com>
2022-08-26 15:49:08 +0600

    add transition and nested options

--- simple-modal.js
+++ simple-modal.js
@@ -4,39 +4,61 @@ class SimpleModal {
 			onOpen: () => {},
 			onClose: () => {},
 			disableScroll: true,
-			transition: 300,
+			transition: 250,
+			nested: true,
+			overlayCloseAll: true,
 		}
 		this.options = Object.assign(defaultOptions, options)
 		this.$html = document.querySelector('html')
 		this.$modals = document.querySelectorAll('.modal')
-		this.transition = 300
+		this.$activeModals = document.querySelectorAll('.modal.is-open')
 		this.isAnimated = false
 	}
 	init() {
 		if (this.$modals.length > 0) {
-			document.body.addEventListener('click', e => {
-				const openTrigger = e.target.closest('[data-modal-open]')
-				const closeTrigger = e.target.closest('[data-modal-close]')
-				const $modal = e.target.closest('.modal')
-				const isOverlay = $modal && !e.target.closest('.modal__inner')
+			this.$modals.forEach($modal => {
+				$modal.style.transitionDuration = this.options.transition / 1000 + 's'
+			})
+			this.events()
+		}
+	}
+	events() {
+		document.body.addEventListener('click', e => {
+			const openTrigger = e.target.closest('[data-modal-open]')
+			const closeTrigger = e.target.closest('[data-modal-close]')
+			const $modal = e.target.closest('.modal')
+			const isOverlay = $modal && !e.target.closest('.modal__inner')
 
-				if (openTrigger) {
-					e.preventDefault()
+			if (openTrigger) {
+				e.preventDefault()
+				const modalId = openTrigger.dataset.modalOpen
 
-					const modalId = openTrigger.dataset.modalOpen
+				if (!this.options.nested && this.$activeModals.length > 0) {
+					this.$activeModals.forEach($item => {
+						this.closeModal($item.id)
+					})
+					setTimeout(() => {
+						this.openModal(modalId)
+					}, this.options.transition)
+				} else {
 					this.openModal(modalId)
-
-				} else if (closeTrigger) {
-					e.preventDefault()
-
-					const modalId = closeTrigger.dataset.modalClose || $modal.id
-					this.closeModal(modalId)
-
-				} else if (isOverlay) {
+				}
+			} else if (closeTrigger) {
+				e.preventDefault()
+				const modalId = closeTrigger.dataset.modalClose || $modal.id
+				this.closeModal(modalId)
+			} else if (isOverlay) {
+				if (this.options.overlayCloseAll && this.$activeModals.length > 0) {
+					this.$activeModals.forEach($item => {
+						this.isAnimated = false
+						this.closeModal($item.id)
+						this.$html.classList.remove('overflow-hidden')
+					})
+				} else {
 					this.closeModal($modal.id)
 				}
-			})
-		}
+			}
+		})
 	}
 	openModal(id) {
 		if (!this.isAnimated) {
@@ -50,31 +72,32 @@ class SimpleModal {
 				if (this.options.disableScroll) {
 					this.$html.classList.add('overflow-hidden')
 				}
-			}, 0)
+			}, 10)
 
 			setTimeout(() => {
 				this.isAnimated = false
-			}, this.transition)
-
-			this.options.onOpen(this)
+				this.$activeModals = document.querySelectorAll('.modal.is-open')
+				this.options.onOpen($modal)
+			}, this.options.transition)
 		}
 	}
 	closeModal(id) {
 		if (!this.isAnimated) {
 			const $modal = document.querySelector('#' + id)
-			this.isAnimated = true
 
+			this.isAnimated = true
 			$modal.classList.remove('is-open')
-			if (this.options.disableScroll) {
+
+			if (this.options.disableScroll && this.$activeModals.length === 1) {
 				this.$html.classList.remove('overflow-hidden')
 			}
 
 			setTimeout(() => {
 				$modal.setAttribute('aria-hidden', true)
 				this.isAnimated = false
-			}, this.transition)
-
-			this.options.onClose(this)
+				this.$activeModals = document.querySelectorAll('.modal.is-open')
+				this.options.onClose($modal)
+			}, this.options.transition)
 		}
 	}
 }
